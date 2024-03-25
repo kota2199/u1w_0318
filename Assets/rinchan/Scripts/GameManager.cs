7754d10dc2596a4373aa74e611040cd7884a9739
Rintaromoirui <131845241+Rintaromoirui@users.noreply.github.com>
2024-03-25 13:46:33 +0900

    SoundManagerの実装

---
+++ Assets/rinchan/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StageType
+{
+    Land,   // 陸のステージ
+    Water   // 水のステージ
+}
+
+public class GameManager : MonoBehaviour
+{
+    public StageType stageType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
